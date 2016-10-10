2bae2b99ccc08d4a2e5df132eabddfc71e17717f
13395381 <conor.kelleher.1@ucdconnect.ie>
2016-10-10 13:34:39 +0100

    Added work plan

--- Python/File Adjusting/search_linebreak.py
+++ Python/File Adjusting/search_linebreak.py
@@ -3,21 +3,8 @@ with open("Full Text.txt", "r") as text:
     errCount = 0
     for line in text:
         line_count += 1
-        #if line_count >= 7695: #remove after finishing whole file
-        #    break
         index = 0
-        for char in line:
-            """Big condition to find possibly problematic formatting
-                first check that the current char is a '.'
-                then check that the line has at least 2 more characters
-                then check that the next character is ' '
-                then check that the following character is an uppercase letter
-                then check that, if there are at least two characters preceeding, that they are not "St" or "Mr"
-                then check that, if there are at least three characters preceeding, that they are not "Mrs"
-                then check that, if there are at least four characters preceding, that they are not "G. E" or "T. E" or "Mlle"
-                then check that, if there are enough characters either side, that it is not the other "." in "G. E." or "T. E."
-                if all these conditions are met, print the line number and check if invalid formatting is found"""
-                
+        for char in line:   
             if (
             (
                 char == "?" and 
